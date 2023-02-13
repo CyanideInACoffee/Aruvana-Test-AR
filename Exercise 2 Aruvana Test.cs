24b80bb7206c7efbec329539dc16d2a9277b6e15
Yana Supriana <122078039+CyanideInACoffee@users.noreply.github.com>
2023-02-13 19:41:41 +0700

    Add files via upload

---
+++ Exercise 2 Aruvana Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_2_Aruvana_Test
+{
+    class Program
+    {
+         static string timeConversion(string s)
+        {
+            return DateTime.Parse(s).ToString("HH:mm:ss");
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(timeConversion("00:12:34 AM"));
+            Console.WriteLine(timeConversion("07:05:45 PM"));
+            Console.Read();
+        }
+    }
+    
+}
